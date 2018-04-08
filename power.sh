5f31d97d4666cd69dbdd82b6964dfa7087eb42fa
Matthew Conniff <conniff.m@husky.neu.edu>
2018-04-08 20:47:10 +0000

    Fixed power script to run with fddb

--- power.sh
+++ power.sh
@@ -1,3 +1,3 @@
 nvidia-smi stats -i 0 -d pwrDraw > device0_pwr.csv& nvidia-smi stats -i 1 -d pwrDraw > device1_pwr.csv&
-./face_detector --log -d ../pics_names.txt ../pics/ -o ./output;
+./face_detector -f -d ../pics_names.txt ../pics/ -o output
 pkill nvidia-smi
