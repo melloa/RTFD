7a4be12ae3d6d9e2eca848c350203501d75fdab5
Andrew Mello <mello.a@husky.neu.edu>
2018-04-27 21:21:02 +0000

    updated script

--- runAll.sh
+++ runAll.sh
@@ -1,9 +1,13 @@
-nvidia-smi stats -i 0 -d pwrDraw > "$1_device0_pwr.csv"& nvidia-smi stats -i 1 -d pwrDraw > "$1_device1_pwr.csv"&
 #!/bin/bash
+
+nvidia-smi stats -i 0 -d pwrDraw > "$1_device0_pwr.csv"& nvidia-smi stats -i 1 -d pwrDraw > "$1_device1_pwr.csv"&
+
 for FILE in pics/*.jpg
 do
         FILENAME_EXT=${FILE##*/}
         NAME=${FILENAME_EXT%%.*}
+        echo $FILE
         ./face_detector -f -i $FILE -o output 2> /dev/null 1> output/${NAME}_output.txt
 done
+
 pkill nvidia-smi
